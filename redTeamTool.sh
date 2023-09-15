0578bb7aa8c9368abcc79210b43b3857bd5167cc
Sean Carroll <69919246+src4413@users.noreply.github.com>
2023-09-15 16:08:18 -0400

    Personal red team tool - HW4

--- redTeamTool.sh
+++ redTeamTool.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [[ $EUID -ne 0 ]]; then
+if [[ $EUID -ne 0 ]]; then #ensures that the user is root
 	echo "Must be run as root."
 	exit 1
 fi
 
-running=$(systemctl list-units --type=service --state=running | tail -n +2 | head -n -4 | awk '{print $1}')
+running=$(systemctl list-units --type=service --state=running | tail -n +2 | head -n -4 | awk '{print $1}') #obtains all the running services on the machine
 
-for service in $running; do
+for service in $running; do #for loop to loop through all the services
 	echo "Stopping: $service"
-	systemctl stop "$service"
+	systemctl stop "$service" #command to stop the service
 done
